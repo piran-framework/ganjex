e86180ccfddc3b038a4c7415fa22592949efe190
esa <esa.hekmat@gmail.com>
2018-02-14 18:11:51 +0330

    create spring boot starter for ganjex

---
+++ ganjex-spring-boot/ganjex-spring-boot-autoconfigure/src/main/java/com/behsa/ganjex/config/GanjexAutoConfiguration.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 Behsa Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.behsa.ganjex.config;
+
+import com.behsa.ganjex.GanjexHook;
+import com.behsa.ganjex.api.Ganjex;
+import com.behsa.ganjex.api.GanjexConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/**
+ * @author hekmatof
+ * @since 1.0
+ */
+@Configuration
+@ConditionalOnBean(GanjexMarkerConfiguration.Marker.class)
+@EnableConfigurationProperties(GanjexProperties.class)
+public class GanjexAutoConfiguration {
+	private static final Logger log = LoggerFactory.getLogger(GanjexAutoConfiguration.class);
+
+	@Bean
+	@ConditionalOnMissingBean
+	public Ganjex ganjex(ApplicationContext applicationContext, GanjexProperties ganjexProperties) {
+		Map<String, Object> hookMap =
+						applicationContext.getBeansWithAnnotation(GanjexHook.class);
+		Ganjex ganjex = Ganjex.run(new GanjexConfiguration.Builder()
+						.servicePath(ganjexProperties.getServicePath())
+						.libPath(ganjexProperties.getLibPath())
+						.watcherDelay(ganjexProperties.getWatchDelay())
+						.hooks(hookMap.values().toArray()).build()
+		);
+		log.info("ganjex container bootstrap finished");
+		log.info("ganjex registered hooks: {}", hookMap.values());
+		return ganjex;
+	}
+}
